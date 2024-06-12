a63ee4cf5d1ddd9057d04dfad5d144df5c644f27
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-12 17:00:37 +0900

    first commit

---
+++ src/main/java/com/searchpharmacy/SearchPharmacyApplication.java
@@ -0,0 +1,13 @@
+package com.searchpharmacy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SearchPharmacyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SearchPharmacyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/searchpharmacy/SearchPharmacyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.searchpharmacy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchPharmacyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
