8024800c3e0bbcceb5f8f454bcfaba56f9655c61
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-13 17:28:05 +0900

    JPA Dirty Checking
    @Transactional이 있을 경우와 없을 경우 차이를 보기위한 테스트

---
+++ src/main/java/com/searchpharmacy/pharmacy/service/PharmacyRepositoryService.java
@@ -0,0 +1,41 @@
+package com.searchpharmacy.pharmacy.service;
+
+import com.searchpharmacy.pharmacy.entity.Pharmacy;
+import com.searchpharmacy.pharmacy.repository.PharmacyRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.Objects;
+
+@Slf4j
+@RequiredArgsConstructor
+@Service
+public class PharmacyRepositoryService {
+    private final PharmacyRepository pharmacyRepository;
+
+    @Transactional
+    public void updateAddress(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+
+    // transectional이 없는 경우 테스트 하기 위한 메소드
+    public void updateAddressWithoutTransaction(Long id, String address) {
+        Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
+
+        if (Objects.isNull(entity)) {
+            log.error("[PharmacyRepositoryService updateAddress] not found id: {}", id);
+            return;
+        }
+
+        entity.changePharmacyAddress(address);
+    }
+}
