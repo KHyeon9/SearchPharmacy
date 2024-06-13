58a18f7f00b5ac0038f2c7c22cade7b0eb36e6d1
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-13 20:34:34 +0900

    self invocatin과 readonly 테스트 작성 및 필요 메소드 정의

--- src/main/java/com/searchpharmacy/pharmacy/service/PharmacyRepositoryService.java
+++ src/main/java/com/searchpharmacy/pharmacy/service/PharmacyRepositoryService.java
@@ -6,7 +6,9 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.TransactionSynchronizationManager;
 
+import java.util.List;
 import java.util.Objects;
 
 @Slf4j
@@ -27,15 +29,43 @@ public class PharmacyRepositoryService {
         entity.changePharmacyAddress(address);
     }
 
+    // self invocation 테스트
+    public void bar(List<Pharmacy> pharmacyList) {
+        log.info("bar CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        foo(pharmacyList);
+    }
+
+    // self invocation 테스트
+    @Transactional
+    public void foo(List<Pharmacy> pharmacyList) {
+        log.info("foo CurrentTransactionName: "+ TransactionSynchronizationManager.getCurrentTransactionName());
+        pharmacyList.forEach(pharmacy -> {
+            pharmacyRepository.save(pharmacy);
+            throw new RuntimeException("error"); // 예외 발생
+        });
+    }
+
+    // read only 테스트
+    @Transactional(readOnly = true)
+    public void startReadOnlyMethod(Long id) {
+        pharmacyRepository.findById(id).ifPresent(pharmacy -> {
+            pharmacy.changePharmacyAddress("서울 특별시 광진구");
+        });
+    }
+
     // transectional이 없는 경우 테스트 하기 위한 메소드
     public void updateAddressWithoutTransaction(Long id, String address) {
         Pharmacy entity = pharmacyRepository.findById(id).orElse(null);
-
         if (Objects.isNull(entity)) {
             log.error("[PharmacyRepositoryService updateAddress] not found id: {}", id);
             return;
         }
-
         entity.changePharmacyAddress(address);
     }
+
+    @Transactional(readOnly = true)
+    public List<Pharmacy> findAll() {
+        return pharmacyRepository.findAll();
+    }
+
 }
