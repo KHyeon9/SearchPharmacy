6c5d1a3d1fb96a1cfb0ca2d71b30437cd2183c33
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-16 20:12:06 +0900

    부트 스트랩, Jquery 적용

---
+++ src/main/java/com/searchpharmacy/direaction/controller/FormController.java
@@ -0,0 +1,15 @@
+package com.searchpharmacy.direaction.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@RequiredArgsConstructor
+@Controller
+public class FormController {
+
+    @GetMapping("/")
+    public String main() {
+        return "main";
+    }
+}
