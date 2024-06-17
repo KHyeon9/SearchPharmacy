9bf510f324bc07f2fcc8b36b4b92f2662a7858e4
git에서 Khyeon9 <git mrk19967@gmail.com>
2024-06-17 20:01:44 +0900

    메인화면, 결과화면 구현

---
+++ src/main/java/com/searchpharmacy/direaction/dto/InputDto.java
@@ -0,0 +1,15 @@
+package com.searchpharmacy.direaction.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class InputDto {
+
+    private String address;
+}
